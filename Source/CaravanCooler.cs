553774f57ec49750470aee8cdac20067c6d6135d
Trevor <tkhowell95@gmail.com>
2020-03-17 13:00:38 -0600

    Created research def for cooler and rearranged research tab items

---
+++ Source/CaravanCooler.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace CaravanCooler
+{
+    [StaticConstructorOnStartup]
+    public class CaravanCooler
+    {
+        static CaravanCooler()
+        {
+            var harmony = new Harmony("com.github.toywalrus.caravancooler");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("Caravan cooler init");
+        }
+    }
+}
