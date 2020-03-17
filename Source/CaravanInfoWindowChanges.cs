9162a82168fb5e3e30f1e0a2dfb33ffe2ea63000
Trevor <tkhowell95@gmail.com>
2020-03-17 16:32:00 -0600

    Added postfix method for checking if food will rot in caravan

---
+++ Source/CaravanInfoWindowChanges.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace CaravanCooler
+{
+    [HarmonyPatch(typeof(Dialog_FormCaravan))]
+    [HarmonyPatch("MostFoodWillRotSoon", MethodType.Getter)]
+    public static class CaravanInfoWindowChanges
+    {
+        [HarmonyPostfix]
+        public static void WillFoodRot(Dialog_FormCaravan __instance, ref bool __result)
+        {
+            Log.Message("Postfix ran!");            
+        }
+    }
+}
