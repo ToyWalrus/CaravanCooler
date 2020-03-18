59d9812ffd1a77adeead09add70c1a7459c8893d
Trevor <tkhowell95@gmail.com>
2020-03-17 19:18:52 -0600

    Added some reference links for help/tutorials

--- Source/CaravanInfoWindowChanges.cs
+++ Source/CaravanInfoWindowChanges.cs
@@ -6,12 +6,19 @@ namespace CaravanCooler
 {
     [HarmonyPatch(typeof(Dialog_FormCaravan))]
     [HarmonyPatch("MostFoodWillRotSoon", MethodType.Getter)]
-    public static class CaravanInfoWindowChanges
+    public static class FormCaravanWarning
     {
         [HarmonyPostfix]
         public static void WillFoodRot(Dialog_FormCaravan __instance, ref bool __result)
         {
             __result = CaravanCooler.WillFoodRot(__instance.transferables);
         }
+    }
+
+    public static class CaravanFormingWindowUtility
+    {
+        // https://github.com/pardeike/Harmony/wiki/Patching
+        // https://harmony.pardeike.net/articles/patching-transpiler.html
+        // public static void NoRotIfCoolerPresent()
     }
 }
