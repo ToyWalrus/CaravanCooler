eae33e39708857d03515d6c7fb4ff4414c913019
Trevor <tkhowell95@gmail.com>
2020-03-19 16:09:28 -0600

    Removed the rot indicator on world map for caravan

---
+++ Source/WorldCaravanInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld.Planet;
+using Verse;
+using System;
+
+namespace CaravanCooler
+{
+    [HarmonyPatch(typeof(DaysUntilRotCalculator))]
+    [HarmonyPatch("ApproxDaysUntilRot", new Type[] {typeof(List<Thing>), typeof(int), typeof(WorldPath), typeof(float), typeof(int)})]
+    public static class WorldCaravanInfo
+    {
+        [HarmonyPostfix]
+        public static void ApproxDaysUntilRot(List<Thing> caravanItems, ref float __result)
+        {
+            for (int i = 0; i < caravanItems.Count; ++i)
+            {
+                if (caravanItems[i].def == CaravanCoolerDefOf.CaravanCooler)
+                {
+                    __result = float.PositiveInfinity;
+                    return;
+                }
+            }
+        }
+    }
+}
